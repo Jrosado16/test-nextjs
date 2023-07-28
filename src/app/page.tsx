884becbbbc084e1b4617dd50dfc9bf57578c348c
Carlos Rosado <krlos_1594@yahoo.com>
2023-07-28 16:58:48 -0600

    redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,25 @@
+'use client'
 import Image from 'next/image'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import dollarIcon from '@/assets/icon/dollar.svg'
 
 export default function Home() {
+  const router = useRouter()
+  const redirect = () => {
+    router.push(`/actions/hhey?name=$repayy`)
+  }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
      <div>
-        
-        <nav>
+        <img src={dollarIcon.src} alt="" />
+        <nav className='flex'>
           <Link href={'/about'}>Aboout</Link>
+          <div className='ml-2'></div>
+          <Link href={`/actions/ñcñcñcñc?name=deposiit`}>
+            Action
+          </Link>
+          <button onClick={redirect} className='ml-2'>repay</button>
         </nav>
      </div>
     </main>
